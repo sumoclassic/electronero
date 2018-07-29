4463c5aeebded26436c937686ba79d9580ca867a
txponto <35672102+txponto@users.noreply.github.com>
2018-07-29 14:52:49 -0500

    Update osx_install.sh

--- osx_install.sh
+++ osx_install.sh
@@ -2,4 +2,4 @@
 brew install gcc@5 cmake pkg-config boost@1.59 boost git openssl@1.1 zmqpp zmq unbound jmuncaster/header-only/cppzmq libsodium miniupnpc libunwind-headers xz readline ldns expat doxygen graphviz
 echo "Dependencies installation complete"
 make
-echo "Electronero Mac Build process complete"
+echo "ElectroneroPulse Mac Build process complete"
